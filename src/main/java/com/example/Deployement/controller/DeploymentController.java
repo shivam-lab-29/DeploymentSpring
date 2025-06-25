1f63c564b6383bfe6af2552e16a8d9fafe135992
shivam gupta <shivamg2211@gmail.com>
2025-06-25 23:01:31 +0530

    controller

---
+++ src/main/java/com/example/Deployement/controller/DeploymentController.java
@@ -0,0 +1,17 @@
+package com.example.Deployement.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class DeploymentController {
+
+    @GetMapping("/d1")
+    public String get()
+    {
+        return "My First CI/CD pipeline";
+    }
+
+}
